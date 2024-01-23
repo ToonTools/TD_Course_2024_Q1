c228d77cdf52690ed659a2fae075b37c87c53fb0
Chris Carter <chrishcarter@gmail.com>
2024-01-23 17:49:44 +0000

    Update TD24Q1_07-3_TextFile_read_write.js

--- scripts/TD24Q1_07-3_TextFile_read_write.js
+++ scripts/TD24Q1_07-3_TextFile_read_write.js
@@ -9,6 +9,23 @@ How to use: launch each of the functions to write and then read information from
 
 function writeTextFile(){
 	MessageLog.trace("this is where text will be written")
+
+	
+	// create text file
+	var myUserName 		= about.userName
+	var myPath 			= "C:\\Users\\" + myUserName + "\\Desktop\\test\\"
+	var fileName 		= "MyTextFile.txt"
+	var myFileNameFull 	= myPath + fileName 
+	
+	MessageLog.trace(myFileNameFull)
+
+	var myFile 			= new File(myFileNameFull)		
+
+	myFile.open(FileAccess.ReadWrite)
+
+	MessageLog.trace("my file should be made now")
+
+	myFile.close()
 }
 
 
