5780b2d78998ab11e0568b1e0f79aa21df3dae94
Chris Carter <chrishcarter@gmail.com>
2024-01-23 17:04:02 +0000

    Update TBTD24Q1_getSceneInformation.js
    return number of nodes selected, adds all nodes to selection, gets that quantity, clears selection

--- scripts/TBTD24Q1_getSceneInformation.js
+++ scripts/TBTD24Q1_getSceneInformation.js
@@ -4,4 +4,15 @@
 function getSceneInformation()
 {
 	MessageLog.trace("started")
+
+	var numberOfNodesSelected = selection.numberOfNodesSelected()
+
+	selection.selectAll()
+
+	var numberOfNodesInScele = selection.numberOfNodesSelected()
+
+	MessageLog.trace("The total number of nodes selected is : " + numberOfNodesSelected )
+	MessageLog.trace("The total number of nodes in the scene  is : " + numberOfNodesInScele )
+
+	selection.clearSelection()
 }
