1ebf89a711301dad6150465ddc1fad8fc827a3b2
Chris Carter <chrishcarter@gmail.com>
2024-01-26 17:20:01 +0000

    Create TD24Q1_10-2_helloFromPython.py

---
+++ scripts/TD24Q1_10-2_helloFromPython.py
@@ -0,0 +1,7 @@
+def helloWorld():
+    message = "hello from python"
+    print("Printing : {}".format(message))
+    return("Returning : {}".format(message))  
+    
+    
+helloWorld() # the file will return something even if the speciffic function is not called
