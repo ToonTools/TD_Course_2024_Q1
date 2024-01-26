91cabcb013a12cbb69f18eeacb4fd2d0576038e4
Chris Carter <chrishcarter@gmail.com>
2024-01-26 17:29:51 +0000

    Create TD24Q1_10-3_tkinterWindow.js

---
+++ scripts/TD24Q1_10-3_tkinterWindow.js
@@ -0,0 +1,55 @@
+var myPythonScript = "C:/Github/ToonTools/TD_Course_2024_Q1/scripts/TD24Q1_10-4_tkinterWindow.py"
+
+// https://docs.toonboom.com/help/harmony-22/scripting/extended/module-PythonManager.html
+function tkinterWindow_pythonManager(){
+	MessageLog.trace("testing: PythonManager")
+
+	var myPythonObject = PythonManager.createPyObject(myPythonScript)
+	var myScriptReturn = myPythonObject.py.simplePythonUI()
+	
+	MessageLog.trace("returning: " + myScriptReturn)
+
+}
+
+// https://doc.qt.io/archives/qt-4.8/qprocess.html
+function tkinterWindow_QProcess(){
+
+  MessageLog.trace("testing: QProcess")
+  var command     = "Python.exe"
+  var arguments   = [myPythonScript]
+  
+  var QPro  = new QProcess();
+  QPro.start(command, arguments)
+  QPro.waitForFinished(-1);
+
+  MessageLog.trace("Output\t: "+ new QTextStream(QPro.readAllStandardOutput()).readAll());
+  MessageLog.trace("Errors\t: "+ new QTextStream(QPro.readAllStandardError()).readAll());
+
+}
+
+
+
+
+//https://docs.toonboom.com/help/harmony-20/scripting/script/classProcess2.html
+function tkinterWindow_Process2(){
+
+  MessageLog.trace("testing: Process2")
+  var command     = "Python.exe " + myPythonScript
+  
+  var Pro2 = new Process2(command)
+  Pro2.launchAndDetach()
+  MessageLog.trace("Command\t: " + Pro2.commandLine());
+  if(Pro2.errorCode()!=0){ // if an error code other than 0 is found, it means there was an error
+    MessageLog.trace("Error Code\t: " + Pro2.errorCode());
+    MessageLog.trace("Error Message\t: " + Pro2.errorMessage());
+  }
+  else{
+    MessageLog.trace("No errors")
+  }
+} 
+
+
+
+
+
+
